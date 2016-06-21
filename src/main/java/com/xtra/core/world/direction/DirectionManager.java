b3435fad1c76ab50a16cb4fc8d5f39be04f25862
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2016-06-21 13:41:08 -0400

    Add DirectionManager from XtraCasino

---
+++ src/main/java/com/xtra/core/world/direction/DirectionManager.java
@@ -0,0 +1,89 @@
+/**
+ * This file is part of XtraCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 - 2016 XtraStudio <https://github.com/XtraStudio>
+ * Copyright (c) Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.xtra.core.world.direction;
+
+import org.spongepowered.api.util.Direction;
+
+public class DirectionManager {
+
+    /**
+     * Gets a cardinal direction from the specified yaw value.
+     * 
+     * @param yaw The yaw value
+     * @return The cardinal direction for the specified yaw value
+     */
+    public static Direction getCardinalDirectionFromYaw(double yaw) {
+        if (yaw >= 135 || yaw <= -135) {
+            return Direction.NORTH;
+        } else if (yaw >= -135 && yaw <= -45) {
+            return Direction.EAST;
+        } else if (yaw >= -45 && yaw <= 45) {
+            return Direction.SOUTH;
+        } else if (yaw >= 45 && yaw <= 135) {
+            return Direction.WEST;
+        }
+        return Direction.NONE;
+    }
+
+    /**
+     * Gets the cardinal left direction from the specified cardinal direction.
+     * 
+     * @param direction The cardinal direction
+     * @return The left cardinal direction from the specified cardinal direction
+     */
+    public static Direction getCardinalLeft(Direction direction) {
+        if (direction.equals(Direction.NORTH)) {
+            return Direction.WEST;
+        } else if (direction.equals(Direction.EAST)) {
+            return Direction.NORTH;
+        } else if (direction.equals(Direction.SOUTH)) {
+            return Direction.EAST;
+        } else if (direction.equals(Direction.WEST)) {
+            return Direction.SOUTH;
+        }
+        return Direction.NONE;
+    }
+
+    /**
+     * Gets the cardinal right direction from the specified cardinal direction.
+     * 
+     * @param direction The cardinal direction
+     * @return The right cardinal direction from the specified cardinal
+     *         direction
+     */
+    public static Direction getCardinalRight(Direction direction) {
+        if (direction.equals(Direction.NORTH)) {
+            return Direction.EAST;
+        } else if (direction.equals(Direction.EAST)) {
+            return Direction.SOUTH;
+        } else if (direction.equals(Direction.SOUTH)) {
+            return Direction.WEST;
+        } else if (direction.equals(Direction.WEST)) {
+            return Direction.NORTH;
+        }
+        return Direction.NONE;
+    }
+}
