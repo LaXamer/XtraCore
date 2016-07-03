6f87b2b70d81991464681cc182d40b1cc4dc6c6b
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2016-07-03 14:38:07 -0400

    Add CommandAnnotationInfo

---
+++ src/main/java/com/xtra/core/command/annotation/CommandAnnotationInfo.java
@@ -0,0 +1,62 @@
+/**
+ * This file is part of XtraCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 - 2016 XtraStudio <https://github.com/XtraStudio>
+ * Copyright (c) Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.xtra.core.command.annotation;
+
+import java.util.Optional;
+
+import com.xtra.core.command.Command;
+import com.xtra.core.command.base.EmptyCommand;
+
+/**
+ * A helper class that gets information from any command annotations.
+ */
+public class CommandAnnotationInfo {
+
+    /**
+     * Returns if the command is an async command.
+     * 
+     * @param clazz The command class to check
+     * @return If the command is async
+     */
+    public static boolean isAsync(Class<? extends Command> clazz) {
+        return clazz.getAnnotation(RegisterCommand.class).async();
+    }
+
+    /**
+     * Gets the parent command of the specified command.
+     * 
+     * @param clazz The command class to check
+     * @return The parent command or {@link Optional#empty()} if the specified
+     *         command does not have a parent command
+     */
+    public static Optional<Class<? extends Command>> getParent(Class<? extends Command> clazz) {
+        Class<? extends Command> parent = clazz.getAnnotation(RegisterCommand.class).childOf();
+        if (!parent.equals(EmptyCommand.class)) {
+            return Optional.of(parent);
+        }
+        return Optional.empty();
+    }
+}
